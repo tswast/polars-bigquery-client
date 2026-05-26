378008fec32d3e22967543690cf2efa66809afa4
Tim Sweña <swast@google.com>
2026-05-26 15:24:04 +0000

    update gcloud-sdk crate

--- crates/polars-bigquery/tests/integration_test.rs
+++ crates/polars-bigquery/tests/integration_test.rs
@@ -1,13 +1,15 @@
 use polars_bigquery::*;
+use std::env;
 
 #[tokio::test(flavor = "multi_thread")]
 async fn test_read_small_public_table() {
+    let quota_project_id = env::var("GOOGLE_CLOUD_PROJECT").expect("must set GOOGLE_CLOUD_PROJECT to run integration tests");
     let result = read_bigquery_async(
         "bigquery-public-data.usa_names.usa_1910_2013",
-        "some-project",
+        &quota_project_id,
         false,
         gcloud_sdk::TokenSourceType::Default,
     )
     .await;
-    assert!(result.is_ok());
+    result.expect("public table read should work with default credentials");
 }
